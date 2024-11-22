5a75474932d11e24ba634a484ebd095b26c04ab3
mayaekie071 <mayaekie071@SDS-L-J9QLG63>
2024-11-22 11:20:38 -0500

    Add project files.

---
+++ Greeting Card/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greeting_Card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen purplePen = new Pen(Color.DarkOrchid, 20);
+            SolidBrush purpleBrush = new SolidBrush(Color.DarkOrchid);
+
+            g.Clear(Color.White);
+            g.DrawEllipse(purplePen, 85, 85, 175, 175);
+            g.FillEllipse(purpleBrush, 85, 85, 175, 175);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
